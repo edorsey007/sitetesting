1625498a2cea6bd1175bac0d2c4214869a7fe437
Graham Dubow <gwd711@gmail.com>
2016-03-15 15:03:30 -0400

    performance improvements

--- gruntfile.js
+++ gruntfile.js
@@ -2,10 +2,25 @@ module.exports = function (grunt) {
   // Configure grunt
   grunt.initConfig({
     sprite:{
-      all: {
-        src: 'img/to-sprite/*.png',
-        dest: 'img/sprites/spritesheet.png',
-        destCss: 'css/sprites.css'
+      cs: {
+        src: 'img/to-sprite-cs/*.png',
+        dest: 'img/sprites/cs/spritesheet.png',
+        destCss: 'css/cs-sprites.css'
+      },
+      icons: {
+        src: 'img/to-sprite-icons/*.png',
+        dest: 'img/sprites/icons/spritesheet.png',
+        destCss: 'css/icons-sprites.css'
+      },
+       partners: {
+        src: 'img/to-sprite-partners/*.png',
+        dest: 'img/sprites/partners/spritesheet.png',
+        destCss: 'css/partners-sprites.css'
+      },
+       team: {
+        src: 'img/to-sprite-team/*.png',
+        dest: 'img/sprites/team/spritesheet.png',
+        destCss: 'css/team-sprites.css'
       }
     }
   });
